354d18538ae029f7be356e34b584bdde9e058fb7
5237-mests <msfnmulgeta@gmail.com>
2025-08-03 04:42:59 +0300

    Static file server using express.

--- src/app.ts
+++ src/app.ts
@@ -8,19 +8,23 @@ import transferRoutes from "./routes/transferRoutes";
 import notificationRoutes from "./routes/notificationRoutes";
 import inventoryRoutes from "./routes/inventoryRoutes";
 import connectDB from "./config/db";
+// import path from "path";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
 
 connectDB();
 
-// Routes
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "../client")));
 
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/check", healthRoutes);
@@ -28,6 +32,11 @@ app.use("/api/shop", shopRoutes);
 app.use("/api/transfer", transferRoutes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/api/inventory", inventoryRoutes);
+
+// Serve the React app for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
 
 // Start the server
 app.listen(PORT, () => {
