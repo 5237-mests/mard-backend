6f537a6f5dd1dd6c67d6f499a80d75ef48dbe03a
5237-mests <msfnmulgeta@gmail.com>
2025-08-10 01:06:50 +0300

    API protected.

--- src/routes/brandRoutes.ts
+++ src/routes/brandRoutes.ts
@@ -1,15 +1,33 @@
 import express from "express";
 import BrandController from "../controllers/brandController";
+import { authenticateToken, authorizeRole } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 // Route to get all brands
-router.get("/all", BrandController.getAllBrands);
+router.get("/all", authenticateToken, BrandController.getAllBrands);
 // Route to create a new brand
-router.post("/create", BrandController.createBrand);
-// Route to delete a brand by ID
-router.delete("/:id", BrandController.deleteBrand);
+router.post(
+  "/create",
+  authenticateToken,
+  authorizeRole(["ADMIN"]),
+  BrandController.createBrand
+);
+
 // Route to update a brand by ID
-router.put("/:id", BrandController.updateBrand);
+router.put(
+  "/:id",
+  authenticateToken,
+  authorizeRole(["ADMIN"]),
+  BrandController.updateBrand
+);
+
+// Route to delete a brand by ID
+router.delete(
+  "/:id",
+  authenticateToken,
+  authorizeRole(["ADMIN"]),
+  BrandController.deleteBrand
+);
 
 export default router;
--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -1,21 +1,37 @@
 import express from "express";
 import categoryController from "../controllers/categoryController";
+import { authenticateToken, authorizeRole } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 // Route to get all categories
-router.get("/all", categoryController.getAllCategories);
+router.get("/all", authenticateToken, categoryController.getAllCategories);
 
 // Route to create a new category
-router.post("/", categoryController.createCategory);
+router.post(
+  "/",
+  authenticateToken,
+  authorizeRole(["ADMIN"]),
+  categoryController.createCategory
+);
 
 // Route to get a category by ID
-router.get("/:id", categoryController.getCategoryById);
+router.get("/:id", authenticateToken, categoryController.getCategoryById);
 
 // Route to update a category by ID
-router.put("/:id", categoryController.updateCategory);
+router.put(
+  "/:id",
+  authenticateToken,
+  authorizeRole(["ADMIN"]),
+  categoryController.updateCategory
+);
 
 // Route to delete a category by ID
-router.delete("/:id", categoryController.deleteCategory);
+router.delete(
+  "/:id",
+  authenticateToken,
+  authorizeRole(["ADMIN"]),
+  categoryController.deleteCategory
+);
 
 export default router;
