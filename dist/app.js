1765c6833ca6481b0fd845dcb7aa147ec8aaba46
5237-mests <msfnmulgeta@gmail.com>
2025-10-02 14:26:31 +0300

    Rebuild the app again for prod.

--- dist/app.js
+++ dist/app.js
@@ -55,6 +55,9 @@ app.use(express_1.default.json());
 // app.use(performanceLogger);
 // Serve static files from the "public" directory
 app.use(express_1.default.static(path_1.default.join(__dirname, "../client")));
+// app.use("/uploads", express.static("public/uploads"));
+// Serve uploads via /uploads URL.
+app.use("/uploads", express_1.default.static(path_1.default.join(process.env.HOME || "/home/mardtryj", "uploads/products")));
 // API Routes
 app.use("/api/auth", authRoutes_1.default);
 app.use("/api/users", userRoutes_1.default);
